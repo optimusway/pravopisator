8bdc3d36f97f56458a8401290d06d344c8f9ccf2
Aleksandr Skakovskiy <optimusway@gmail.com>
2024-04-14 22:05:49 +0500

    seed

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import "./global.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -25,5 +28,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <Theme accentColor="green">
+      <Outlet />
+    </Theme>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,152 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { CheckIcon, CopyIcon, InfoCircledIcon } from "@radix-ui/react-icons";
+import {
+  Blockquote,
+  Box,
+  Button,
+  Callout,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Separator,
+  Text,
+  TextArea,
+} from "@radix-ui/themes";
+import {
+  json,
+  type ActionFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/cloudflare";
+import { Form, useActionData, useFetcher } from "@remix-run/react";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Peshi – Помогатор по правописанию" },
     {
       name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
+      content:
+        "Правописатор исправляет ошибки и опечатки, расставляет запятые и знаки препинания.",
     },
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const text = String(formData.get("text"));
+
+  if (!text) {
+    return json({
+      error: "Введите текст",
+      result: "",
+    });
+  }
+
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/ai-forever/sage-fredt5-distilled-95m",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_TOKEN}`,
+      },
+      method: "POST",
+      body: JSON.stringify({ inputs: text }),
+    },
+  );
+
+  const result = await response.json();
+  const error = result?.error ? String(result?.error) : "";
+
+  return json({
+    error: error.includes("loading")
+      ? "Модель загружается. Попробуйте через несколько секунд"
+      : error,
+    result: Array.isArray(result) ? String(result[0]?.generated_text) : "",
+  });
+}
+
 export default function Index() {
+  const fetcher = useFetcher();
+  const actionData = useActionData<typeof action>();
+  const isReady = fetcher.state === "idle" && actionData;
+
+  const [copied, setCopied] = useState("Скопировать");
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Container size="1" height={"100vh"}>
+      <Flex direction={"column"} height={"100%"} px="2" pt={"4"} pb={"2"}>
+        <Heading>Правописатор</Heading>
+        <Text>
+          Исправляет ошибки и опечатки, расставляет запятые и знаки препинания.
+        </Text>
+        <Box mt="2">
+          <Form method="POST">
+            <TextArea name="text" />
+            <Flex justify={"end"}>
+              <Button type="submit" mt={"2"}>
+                <CheckIcon />
+                <Text>Исправить</Text>
+              </Button>
+            </Flex>
+          </Form>
+        </Box>
+
+        {isReady ? (
+          <Box mt={"4"}>
+            {actionData?.error ? (
+              <Callout.Root color="red">
+                <Callout.Icon>
+                  <InfoCircledIcon />
+                </Callout.Icon>
+                <Callout.Text>{actionData?.error}</Callout.Text>
+              </Callout.Root>
+            ) : null}
+
+            {actionData?.result ? (
+              <Box>
+                <Heading size={"4"}>Без ошибок и с запятыми 👇</Heading>
+                <Flex direction={"column"} gap={"2"} mt="2">
+                  <Blockquote size={"3"}>{actionData?.result}</Blockquote>
+                  <Flex align={"center"} justify={"end"} gap={"2"}>
+                    <Button
+                      variant="soft"
+                      size={"2"}
+                      onClick={async () => {
+                        if (navigator.clipboard) {
+                          await navigator.clipboard.writeText(
+                            actionData.result,
+                          );
+                          setCopied("Скопировано");
+                          setTimeout(() => {
+                            setCopied("Скопировать");
+                          }, 3000);
+                        }
+                      }}
+                    >
+                      <CopyIcon />
+                      {copied}
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Box>
+            ) : null}
+          </Box>
+        ) : null}
+
+        <Flex asChild mt={"auto"} justify={"center"} align={"center"}>
+          <footer>
+            <Text size={"2"}>
+              <Link href="https://x.com/optimusway" size={"2"}>
+                @optimusway
+              </Link>
+            </Text>
+            <Separator orientation={"vertical"} mx={"2"} />
+            <Text size={"2"}>
+              Работает на{" "}
+              <Link href="https://github.com/ai-forever/sage">SAGE</Link>
+            </Text>
+          </footer>
+        </Flex>
+      </Flex>
+    </Container>
   );
 }
